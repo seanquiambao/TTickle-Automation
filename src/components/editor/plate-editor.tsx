0d33087e6f5448802e8d4b6d33367eb944b71524
stormyy00 <jonathan.trujillo0432@gmail.com>
2025-02-21 12:25:53 -0800

    added saving and fetching

--- src/components/editor/plate-editor.tsx
+++ src/components/editor/plate-editor.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import { useState } from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
@@ -10,16 +10,26 @@ import { useCreateEditor } from "@/components/editor/use-create-editor";
 import { SettingsDialog } from "@/components/editor/settings";
 import { Editor, EditorContainer } from "@/components/plate-ui/editor";
 
-export function PlateEditor() {
+export function PlateEditor({
+  onChange,
+}: {
+  onChange: (value: string) => void;
+}) {
   const editor = useCreateEditor();
 
   return (
     <DndProvider backend={HTML5Backend}>
-      <Plate editor={editor}>
+      <Plate
+        editor={editor}
+        onChange={() => {
+          onChange(
+            editor.children.map((child) => child.children[0].text).join("\n"),
+          );
+        }}
+      >
         <EditorContainer>
           <Editor variant="default" />
         </EditorContainer>
-
         <SettingsDialog />
       </Plate>
     </DndProvider>
